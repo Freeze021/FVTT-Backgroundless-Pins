6f41df1497ad4193085cbaa50d5c1471b24eb58b
Freeze021 <84507769+Freeze021@users.noreply.github.com>
2022-09-17 20:03:43 +0200

    Update backgroundless-pins.js

--- backgroundless-pins.js
+++ backgroundless-pins.js
@@ -1,8 +1,8 @@
 Hooks.on("init", () => {
     // Override map notes to use the BackgroundlessControlIcon
     Note.prototype._drawControlIcon = function () {
-        let tint = this.data.iconTint ? colorStringToHex(this.data.iconTint) : null;
-        let iconData = { texture: this.data.icon, size: this.size, tint: tint };
+        let tint = Color.from(this.document.texture.tint || null);
+        let iconData = { texture: this.document.texture.src, size: this.size, tint: tint };
         let icon;
         if (this.document.getFlag("backgroundless-pins", "hasBackground")) {
             icon = new ControlIcon(iconData);
@@ -16,8 +16,8 @@ Hooks.on("init", () => {
 });
 
 Hooks.on("renderNoteConfig", (noteConfig, html, _) => {
-    const hasBackground = noteConfig.object.getFlag("backgroundless-pins", "hasBackground") ?? false;
-    const iconTintGroup = html.find("[name=iconTint]").closest(".form-group");
+    const hasBackground = noteConfig.document.getFlag("backgroundless-pins", "hasBackground") ?? false;
+    const iconTintGroup = html.find("[name='texture.tint']").closest(".form-group");
     iconTintGroup.after(`
         <div class="form-group">
             <label for="flags.backgroundless-pins.hasBackground">Show Background?</label>
@@ -32,6 +32,9 @@ export class BackgroundlessControlIcon extends ControlIcon {
      * Override ControlIcon#draw to remove drawing of the background.
      */
     async draw() {
+		// Load the icon texture
+		this.texture = this.texture ?? await loadTexture(this.iconSrc);
+
         // Draw border
         this.border
             .clear()
@@ -41,7 +44,7 @@ export class BackgroundlessControlIcon extends ControlIcon {
         this.border.visible = false;
 
         // Draw icon
-        this.icon.texture = this.texture ?? (await loadTexture(this.iconSrc));
+        this.icon.texture = this.texture;
         this.icon.width = this.icon.height = this.size;
         this.icon.tint = Number.isNumeric(this.tintColor) ? this.tintColor : 0xffffff;
         return this;
